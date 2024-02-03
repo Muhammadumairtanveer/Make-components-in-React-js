2c9972f55c9af63d2a9ce2a16a3b0511ea864c54
Muhammadumairtanveer <umair799284@gmail.com>
2024-02-03 12:41:33 +0500

    to-do-list

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import AppName from "./components/AppName"
+import Task from "./components/Task"
+import "./App.css";
+
+function App() {
+ 
+  return <center className="todo-container" >
+    
+    <AppName/>
+    <Task></Task>
+ 
+
+  
+
+    
+  
+    
+    </center>
+
+  
+}
+
+export default App
